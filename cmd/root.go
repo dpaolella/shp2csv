8b37ef64b21bd8d3865b7331b5505ba0f3bc30e2
dpaolella <dpaolella25@gmail.com>
2017-08-04 15:04:59 -0700

    removed unused code

--- cmd/root.go
+++ cmd/root.go
@@ -39,33 +39,3 @@ func Execute() {
 		os.Exit(-1)
 	}
 }
-
-//
-// func init() {
-// 	cobra.OnInitialize(initConfig)
-//
-// 	// Here you will define your flags and configuration settings.
-// 	// Cobra supports Persistent Flags, which, if defined here,
-// 	// will be global for your application.
-//
-// 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.shp2csv.yaml)")
-// 	// Cobra also supports local flags, which will only run
-// 	// when this action is called directly.
-// 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-// }
-//
-// // initConfig reads in config file and ENV variables if set.
-// func initConfig() {
-// 	if cfgFile != "" { // enable ability to specify config file via flag
-// 		viper.SetConfigFile(cfgFile)
-// 	}
-//
-// 	viper.SetConfigName(".shp2csv") // name of config file (without extension)
-// 	viper.AddConfigPath("$HOME")       // adding home directory as first search path
-// 	viper.AutomaticEnv()               // read in environment variables that match
-//
-// 	// If a config file is found, read it in.
-// 	if err := viper.ReadInConfig(); err == nil {
-// 		fmt.Println("Using config file:", viper.ConfigFileUsed())
-// 	}
-// }
--- cmd/runDir.go
+++ cmd/runDir.go
@@ -32,15 +32,4 @@ var runDirCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(runDirCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runDirCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runDirCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
